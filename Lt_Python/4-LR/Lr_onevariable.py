54c51d7999b56c53a6b7423091e7e89fa500127b
Nhat Phi <syyrex090904@gmail.com>
2024-10-15 02:24:53 +0700

    update

--- Lt_Python/4-LR/Lr_onevariable.py
+++ Lt_Python/4-LR/Lr_onevariable.py
@@ -2,9 +2,7 @@ import numpy as np
 
 def linear_regression(X, y):
     X = np.column_stack((np.ones(X.shape[0]), X))
-    
     theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
-    
     return theta[1], theta[0] 
 
 def main():
@@ -23,9 +21,7 @@ def main():
 
     X = np.array(X)
     y = np.array(y)
-
     a, b = linear_regression(X, y)
-
     print(f"{a} {b}")
 
 if __name__ == "__main__":
